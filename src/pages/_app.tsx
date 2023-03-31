da2db1b8631a80b117b17de636c37700f0d67b9a
soutovp <fernandoandradesouto@hotmail.com>
2023-03-31 11:38:23 -0300

    asd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import '@styles/styles.css.ts'
+import '@styles/styles'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />
